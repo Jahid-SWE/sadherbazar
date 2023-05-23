6738dabe0150b71430b085f05979ccbdd6b01a50
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-05-23 22:17:45 +0600

    new footer file change

---
+++ resources/views/backend/includes/footer.blade.php
@@ -0,0 +1,15 @@
+{{-- <div class="container-fluid px-4  ">
+    <div class="d-flex align-items-center justify-content-between small">
+        <div class="text-muted text-center ">Copyright &copy; Your Website 2023</div>
+        <div>
+            <a href="#">Privacy Policy</a>
+            &middot;
+            <a href="#">Terms &amp; Conditions</a>
+        </div>
+    </div>
+</div> --}}
+<div class="container">
+    <div class="row">
+        <div class="text-center text-white"> Copyright &copy;<span>U_Soft_IT 2023</span> </div>
+    </div>
+</div>
