63c93426244a2f9b6093e37a5655e6f4231e9078
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-05-24 23:55:28 +0600

    change design

--- resources/views/backend/includes/site_nav.blade.php
+++ resources/views/backend/includes/site_nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="sb-sidenav accordion sb-sidenav-dark bg-primary" id="sidenavAccordion">
     <div class="sb-sidenav-menu">
         <div class="nav">
-            <div class="sb-sidenav-menu-heading">Core</div>
+            <div class="sb-sidenav-menu-heading text-light">Core</div>
             <a class="nav-link" href="index.html">
                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                 Dashboard
@@ -50,7 +50,7 @@
                     </div>
                 </nav>
             </div>
-            <div class="sb-sidenav-menu-heading">Addons</div>
+            <div class="sb-sidenav-menu-heading text-light">Addons</div>
             <a class="nav-link" href="charts.html">
                 <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                 Charts
